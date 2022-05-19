515cb4a8d377796dbc7d1f7d212137e6ef3a3af7
Raj <rajkumer2001@gmail.com>
2022-05-19 10:53:27 +0600

    laravel crud with vue

---
+++ resources/js/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter,createWebHistory } from "vue-router";
+import AllProduct from '../components/AllProduct.vue';
+import CreateProduct from '../components/CreateProduct.vue';
+import EditProduct from '../components/EditProduct.vue';
+
+
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: AllProduct
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateProduct
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditProduct
+    }
+];
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+});
+export default router;
